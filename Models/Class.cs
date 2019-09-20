22a142cccbd992d8b208fb20b367eb9bd1498da0
Jenna Weaver <jw537414@ohio.edu>
2019-09-19 22:11:34 -0400

    commit class, student, teacher built

---
+++ Models/Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using jw537414_MIS_plswrk.Models; // This is needed to access the models
+using System.Data.Entity; // this is needed to access the DbContext object
+using System.ComponentModel.DataAnnotations;
+
+
+public class Class
+{
+    [Key]
+    public int classID { get; set; }
+    public string className { get; set; }
+    public string description { get; set; }
+
+
+
+    public int studentID { get; set; }
+    public virtual Student Student { get; set; }
+    // the next two properties link the orderDetail to the Product
+    public int teacherID { get; set; }
+    public virtual Teacher Teacher { get; set; }
+ }
---
+++ Models/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace jw537414_MIS_plswrk.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int teacherID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        public ICollection<Class> Class { get; set; }
+    }
+}
