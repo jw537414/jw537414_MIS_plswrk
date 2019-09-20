ac2b9b0f6b726e1400703e6fe1b99a6bba4230d2
Jenna Weaver <jw537414@ohio.edu>
2019-09-20 10:01:11 -0400

    Commit SD2

---
+++ Controllers/ClassesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using jw537414_MIS_plswrk.DAL;
+
+namespace jw537414_MIS_plswrk.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private MIS4200Context db = new MIS4200Context();
+
+        // GET: Classes
+        public ActionResult Index()
+        {
+            var classes = db.Classes.Include(C => C.Student).Include(T => T.Teacher);
+            return View(classes.ToList());
+        }
+
+        // GET: Classes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Classes.Find(id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+        // GET: Classes/Create
+        public ActionResult Create()
+        {
+            ViewBag.studentID = new SelectList(db.Students, "studentID", "firstName");
+            ViewBag.teacherID = new SelectList(db.Teachers, "teacherID", "firstName");
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "classID,className,description,studentID,teacherID")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Classes.Add(@class);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.studentID = new SelectList(db.Students, "studentID", "firstName", @class.studentID);
+            ViewBag.teacherID = new SelectList(db.Teachers, "teacherID", "firstName", @class.teacherID);
+            return View(@class);
+        }
+
+        // GET: Classes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Classes.Find(id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.studentID = new SelectList(db.Students, "studentID", "firstName", @class.studentID);
+            ViewBag.teacherID = new SelectList(db.Teachers, "teacherID", "firstName", @class.teacherID);
+            return View(@class);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "classID,className,description,studentID,teacherID")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@class).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.studentID = new SelectList(db.Students, "studentID", "firstName", @class.studentID);
+            ViewBag.teacherID = new SelectList(db.Teachers, "teacherID", "firstName", @class.teacherID);
+            return View(@class);
+        }
+
+        // GET: Classes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Class @class = db.Classes.Find(id);
+            if (@class == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@class);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Class @class = db.Classes.Find(id);
+            db.Classes.Remove(@class);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Models/Class.cs
+++ Models/Class.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using jw537414_MIS_plswrk.Models; // This is needed to access the models
-using System.Data.Entity; // this is needed to access the DbContext object
+﻿using jw537414_MIS_plswrk.Models;
 using System.ComponentModel.DataAnnotations;
-
 
 public class Class
 {
