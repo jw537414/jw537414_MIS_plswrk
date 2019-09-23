35f4491882f40869aa2ab6bc9490aa967e2d9309
Jenna Weaver <jw537414@ohio.edu>
2019-09-23 10:28:27 -0400

    ic 9/23

--- Models/Student.cs
+++ Models/Student.cs
@@ -9,13 +9,22 @@ namespace jw537414_MIS_plswrk.Models
 {
     public class Student
     {
-        [Key]
+        [Display(Name = "Student ID")]
         public int studentID { get; set; }
+
+        [Display(Name = "Student first name")]
         public string firstName { get; set; }
+
+        [Display(Name = "Student last name")]
         public string lastName { get; set; }
+
+        [Display(Name = "Student email")]
         public string email { get; set; }
+
+        [Display(Name = "Student phone number")]
         public string phone { get; set; }
 
-        public ICollection<Class> Class { get; set; }
+        public ICollection<Class> Class { get; set; }
+
     }
 }
